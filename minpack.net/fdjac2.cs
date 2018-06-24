cc7971bb3747362931007b798d53e18516581e19
katase <weive735@gmail.com>
2018-06-25 03:13:39 +0900

    revert all codes to original

--- minpack.net/fdjac2.cs
+++ minpack.net/fdjac2.cs
@@ -1,135 +1,122 @@
-using static System.Math;
-using static MinpackDotNet.Dpmar;
+#include "cminpack.h"
+#include <math.h>
+#include "cminpackP.h"
 
-namespace MinpackDotNet
+__cminpack_attr__
+int __cminpack_func__(fdjac2)(__cminpack_decl_fcn_mn__ void *p, int m, int n, real *x, 
+	const real *fvec, real *fjac, int ldfjac,
+	real epsfcn, real *wa)
 {
+    /* Local variables */
+    real h;
+    int i, j;
+    real eps, temp, epsmch;
+    int iflag;
 
-    partial class Fdjac
-    {
-        //__cminpack_attr__
-        //int __cminpack_func__(fdjac2)(__cminpack_decl_fcn_mn__ void* p, int m, int n, real *x, 
-        internal static int fdjac2(cminpack_func_mn fcn_mn, void* p, int m, int n, double[] x,
-            double[] fvec, ref double[] fjac, int ldfjac,
-            double epsfcn, out double wa)
-        {
+/*     ********** */
 
-            /* Local variables */
-            double h;
-            int i, j;
-            double eps, temp, epsmch;
-            int iflag;
+/*     subroutine fdjac2 */
 
-            /*     ********** */
+/*     this subroutine computes a forward-difference approximation */
+/*     to the m by n jacobian matrix associated with a specified */
+/*     problem of m functions in n variables. */
 
-            /*     subroutine fdjac2 */
+/*     the subroutine statement is */
 
-            /*     this subroutine computes a forward-difference approximation */
-            /*     to the m by n jacobian matrix associated with a specified */
-            /*     problem of m functions in n variables. */
+/*       subroutine fdjac2(fcn,m,n,x,fvec,fjac,ldfjac,iflag,epsfcn,wa) */
 
-            /*     the subroutine statement is */
+/*     where */
 
-            /*       subroutine fdjac2(fcn,m,n,x,fvec,fjac,ldfjac,iflag,epsfcn,wa) */
+/*       fcn is the name of the user-supplied subroutine which */
+/*         calculates the functions. fcn must be declared */
+/*         in an external statement in the user calling */
+/*         program, and should be written as follows. */
 
-            /*     where */
+/*         subroutine fcn(m,n,x,fvec,iflag) */
+/*         integer m,n,iflag */
+/*         double precision x(n),fvec(m) */
+/*         ---------- */
+/*         calculate the functions at x and */
+/*         return this vector in fvec. */
+/*         ---------- */
+/*         return */
+/*         end */
 
-            /*       fcn is the name of the user-supplied subroutine which */
-            /*         calculates the functions. fcn must be declared */
-            /*         in an external statement in the user calling */
-            /*         program, and should be written as follows. */
+/*         the value of iflag should not be changed by fcn unless */
+/*         the user wants to terminate execution of fdjac2. */
+/*         in this case set iflag to a negative integer. */
 
-            /*         subroutine fcn(m,n,x,fvec,iflag) */
-            /*         integer m,n,iflag */
-            /*         double precision x(n),fvec(m) */
-            /*         ---------- */
-            /*         calculate the functions at x and */
-            /*         return this vector in fvec. */
-            /*         ---------- */
-            /*         return */
-            /*         end */
+/*       m is a positive integer input variable set to the number */
+/*         of functions. */
 
-            /*         the value of iflag should not be changed by fcn unless */
-            /*         the user wants to terminate execution of fdjac2. */
-            /*         in this case set iflag to a negative integer. */
+/*       n is a positive integer input variable set to the number */
+/*         of variables. n must not exceed m. */
 
-            /*       m is a positive integer input variable set to the number */
-            /*         of functions. */
+/*       x is an input array of length n. */
 
-            /*       n is a positive integer input variable set to the number */
-            /*         of variables. n must not exceed m. */
+/*       fvec is an input array of length m which must contain the */
+/*         functions evaluated at x. */
 
-            /*       x is an input array of length n. */
+/*       fjac is an output m by n array which contains the */
+/*         approximation to the jacobian matrix evaluated at x. */
 
-            /*       fvec is an input array of length m which must contain the */
-            /*         functions evaluated at x. */
+/*       ldfjac is a positive integer input variable not less than m */
+/*         which specifies the leading dimension of the array fjac. */
 
-            /*       fjac is an output m by n array which contains the */
-            /*         approximation to the jacobian matrix evaluated at x. */
+/*       iflag is an integer variable which can be used to terminate */
+/*         the execution of fdjac2. see description of fcn. */
 
-            /*       ldfjac is a positive integer input variable not less than m */
-            /*         which specifies the leading dimension of the array fjac. */
+/*       epsfcn is an input variable used in determining a suitable */
+/*         step length for the forward-difference approximation. this */
+/*         approximation assumes that the relative errors in the */
+/*         functions are of the order of epsfcn. if epsfcn is less */
+/*         than the machine precision, it is assumed that the relative */
+/*         errors in the functions are of the order of the machine */
+/*         precision. */
 
-            /*       iflag is an integer variable which can be used to terminate */
-            /*         the execution of fdjac2. see description of fcn. */
+/*       wa is a work array of length m. */
 
-            /*       epsfcn is an input variable used in determining a suitable */
-            /*         step length for the forward-difference approximation. this */
-            /*         approximation assumes that the relative errors in the */
-            /*         functions are of the order of epsfcn. if epsfcn is less */
-            /*         than the machine precision, it is assumed that the relative */
-            /*         errors in the functions are of the order of the machine */
-            /*         precision. */
+/*     subprograms called */
 
-            /*       wa is a work array of length m. */
+/*       user-supplied ...... fcn */
 
-            /*     subprograms called */
+/*       minpack-supplied ... dpmpar */
 
-            /*       user-supplied ...... fcn */
+/*       fortran-supplied ... dabs,dmax1,dsqrt */
 
-            /*       minpack-supplied ... dpmpar */
+/*     argonne national laboratory. minpack project. march 1980. */
+/*     burton s. garbow, kenneth e. hillstrom, jorge j. more */
 
-            /*       fortran-supplied ... dabs,dmax1,dsqrt */
+/*     ********** */
 
-            /*     argonne national laboratory. minpack project. march 1980. */
-            /*     burton s. garbow, kenneth e. hillstrom, jorge j. more */
+/*     epsmch is the machine precision. */
 
-            /*     ********** */
+    epsmch = __cminpack_func__(dpmpar)(1);
 
-            /*     epsmch is the machine precision. */
-
-            epsmch = dpmpar(1);
-
-            eps = Sqrt((Max(epsfcn, epsmch)));
-            for (j = 0; j < n; ++j)
-            {
-                temp = x[j];
-                h = eps * Abs(temp);
-                if (h == 0d)
-                {
-                    h = eps;
-                }
-                x[j] = temp + h;
-                /* the last parameter of fcn_mn() is set to 2 to differentiate
-                   calls made to compute the function from calls made to compute
-                   the Jacobian (see fcn() in examples/lmfdrv.c, and how njev
-                   is used to compute the number of Jacobian evaluations) */
-                //iflag = fcn_mn(p, m, n, x, wa, 2);
-                iflag = fcn_mn(p, m, n, x, wa, 2);
-                if (iflag < 0)
-                {
-                    return iflag;
-                }
-                x[j] = temp;
-                for (i = 0; i < m; ++i)
-                {
-                    fjac[i + j * ldfjac] = (wa[i] - fvec[i]) / h;
-                }
-            }
-            return 0;
-
-            /*     last card of subroutine fdjac2. */
-
-        } /* fdjac2_ */
-
+    eps = sqrt((max(epsfcn,epsmch)));
+    for (j = 0; j < n; ++j) {
+	temp = x[j];
+	h = eps * fabs(temp);
+	if (h == 0.) {
+	    h = eps;
+	}
+	x[j] = temp + h;
+        /* the last parameter of fcn_mn() is set to 2 to differentiate
+           calls made to compute the function from calls made to compute
+           the Jacobian (see fcn() in examples/lmfdrv.c, and how njev
+           is used to compute the number of Jacobian evaluations) */
+	iflag = fcn_mn(p, m, n, x, wa, 2);
+	if (iflag < 0) {
+            return iflag;
+	}
+	x[j] = temp;
+	for (i = 0; i < m; ++i) {
+	    fjac[i + j * ldfjac] = (wa[i] - fvec[i]) / h;
+	}
     }
-}
+    return 0;
+
+/*     last card of subroutine fdjac2. */
+
+} /* fdjac2_ */
+
